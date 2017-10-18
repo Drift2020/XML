f2bacd5e5c9bc3fb8d8f80c609386c6b9e9a35fd
Mamory <32417036+Drift2020@users.noreply.github.com>
2017-10-18 20:44:55 +0300

    Save and Load

--- ConsoleSeting/ConsoleSeting/Application_Settings_Helper.cs
+++ ConsoleSeting/ConsoleSeting/Application_Settings_Helper.cs
@@ -2,11 +2,80 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Xml;
 
 namespace ConsoleSeting
 {
     class Application_Settings_Helper
     {
+        static Application_Settings_Helper()
+        {
+            ColorFonConsole = Console.BackgroundColor.ToString();
+            ColorTextConsole = Console.ForegroundColor.ToString();
+            size.H = Console.WindowHeight;
+            size.W = Console.WindowWidth;
+            StartTextConsole = "C#";
+        }
+        public static string ColorFonConsole { get; set; }
+        public static string ColorTextConsole { get; set; }
+
+        public static SizeWindow size = new SizeWindow();
+
+        public static string StartTextConsole { get; set; }
+
+        public static void Save()
+        {
+            XmlTextWriter xmlwriter = new XmlTextWriter("Configs.xml", Encoding.UTF8);
+            xmlwriter.WriteStartDocument();
+            xmlwriter.Formatting = Formatting.Indented;
+            xmlwriter.IndentChar = '\t'; // Возвращает или задает знак для отступа
+            xmlwriter.Indentation = 1;
+            xmlwriter.WriteStartElement("Config");
+            xmlwriter.WriteStartElement("Console");
+
+            xmlwriter.WriteStartAttribute("ColorFonConsole", null);
+            xmlwriter.WriteString(ColorFonConsole);
+            xmlwriter.WriteEndAttribute();
+
+            xmlwriter.WriteStartAttribute("ColorTextConsole", null); // Записывает атрибут с заданным именем
+            xmlwriter.WriteString(ColorTextConsole); // Записывает заданное текстовое содержимое атрибута
+            xmlwriter.WriteEndAttribute();
+
+            xmlwriter.WriteStartAttribute("size.H", null); // Записывает атрибут с заданным именем
+            xmlwriter.WriteString(size.H.ToString()); // Записывает заданное текстовое содержимое атрибута
+            xmlwriter.WriteEndAttribute();
+
+            xmlwriter.WriteStartAttribute("size.W", null); // Записывает атрибут с заданным именем
+            xmlwriter.WriteString(size.W.ToString()); // Записывает заданное текстовое содержимое атрибута
+            xmlwriter.WriteEndAttribute();
+
+            xmlwriter.WriteStartAttribute("StartTextConsole", null); // Записывает атрибут с заданным именем
+            xmlwriter.WriteString(StartTextConsole); // Записывает заданное текстовое содержимое атрибута
+            xmlwriter.WriteEndAttribute();
+
+            xmlwriter.WriteEndElement(); 
+            xmlwriter.WriteEndElement();
+            xmlwriter.Close();
+
+           
+
+        }
+        public static void Load()
+        {
+            XmlDocument xDoc = new XmlDocument();
+            xDoc.Load("Configs.xml");
+
+            XmlElement xRoot = xDoc.DocumentElement;
+         
+            XmlNodeList list = xRoot.SelectNodes("Console");
+            foreach (XmlNode n in list)
+            {
+                ColorFonConsole = (n.SelectSingleNode("@ColorFonConsole").Value);
+                ColorTextConsole = (n.SelectSingleNode("@ColorTextConsole").Value);
+                size.H = Int32.Parse(n.SelectSingleNode("@size.H").Value);
+                size.W = Int32.Parse(n.SelectSingleNode("@size.W").Value);
+                StartTextConsole = (n.SelectSingleNode("@StartTextConsole").Value);               
+            }
+        }
     }
 }
--- ConsoleSeting/ConsoleSeting/Start.cs
+++ ConsoleSeting/ConsoleSeting/Start.cs
@@ -8,9 +8,70 @@ namespace ConsoleSeting
 {
     class Start
     {
+        public static void print()
+        {
+            Console.WriteLine("Изменить цвет фона - 1");
+            Console.WriteLine("Изменить цвет текста - 2");
+            Console.WriteLine("Изменить размер окна - 3");
+            Console.WriteLine("Изменить заголовок - 4");
+            Console.WriteLine("Сохранить настройки - 5");
+            Console.WriteLine("Загрузить настройки - 6");
+            Console.WriteLine("Выход - 666");
+            Console.Write("Ввод:");
+        }
         public static void Main()
         {
+            try
+            {
+                bool worc = true;
+                do
+                {
+                    try
+                    {
 
+                        print();
+                        int numbers = 0;
+                        numbers = Convert.ToInt32(Console.ReadLine());
+                        switch (numbers)
+                        {
+                            case 1:
+                                Console.Clear();
+
+                                Application_Settings_Helper.ColorFonConsole = (UserInterface.ColorFon());
+                                break;
+                            case 2:
+                                Console.Clear();
+                                Application_Settings_Helper.ColorTextConsole = (UserInterface.ColorText());
+                                break;
+                            case 3:
+                                Console.Clear();
+                                Application_Settings_Helper.size = (UserInterface.SizeWindows());
+                                break;
+                            case 4:
+                                Console.Clear();
+                                Application_Settings_Helper.StartTextConsole = (UserInterface.StartText());
+                                break;
+                            case 5:
+                                Console.Clear();
+                                Application_Settings_Helper.Save();
+                                break;
+                            case 6:
+                                Console.Clear();
+                                Application_Settings_Helper.Load();
+                                break;
+                            case 666:
+                                Console.Clear();
+                                worc = false;
+                                break;
+                            default:
+                                Console.Clear();
+                                break;
+                        };
+                    }
+                    catch (Exception ex) { Console.Clear(); Console.WriteLine(ex); }
+                } while (worc);
+            }
+            catch (Exception ex) { Console.WriteLine(ex); }
         }
     }
 }
