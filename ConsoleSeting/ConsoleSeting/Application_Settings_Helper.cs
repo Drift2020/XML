dd459b0fe355bf34c2f9ec895eff0a742e9c7302
Drift2020 <linkor1888@gmail.com>
2017-10-19 14:59:17 +0300

    s

--- ConsoleSeting/ConsoleSeting/Application_Settings_Helper.cs
+++ ConsoleSeting/ConsoleSeting/Application_Settings_Helper.cs
@@ -26,37 +26,39 @@ namespace ConsoleSeting
         public static void Save()
         {
             XmlTextWriter xmlwriter = new XmlTextWriter("Configs.xml", Encoding.UTF8);
-            xmlwriter.WriteStartDocument();
-            xmlwriter.Formatting = Formatting.Indented;
-            xmlwriter.IndentChar = '\t'; // Возвращает или задает знак для отступа
-            xmlwriter.Indentation = 1;
-            xmlwriter.WriteStartElement("Config");
-            xmlwriter.WriteStartElement("Console");
+            if(xmlwriter!=null)
+            {
+                xmlwriter.WriteStartDocument();
+                xmlwriter.Formatting = Formatting.Indented;
+                xmlwriter.IndentChar = '\t'; // Возвращает или задает знак для отступа
+                xmlwriter.Indentation = 1;
+                xmlwriter.WriteStartElement("Config");
+                xmlwriter.WriteStartElement("Console");
 
-            xmlwriter.WriteStartAttribute("ColorFonConsole", null);
-            xmlwriter.WriteString(ColorFonConsole);
-            xmlwriter.WriteEndAttribute();
+                xmlwriter.WriteStartAttribute("ColorFonConsole", null);
+                xmlwriter.WriteString(ColorFonConsole);
+                xmlwriter.WriteEndAttribute();
 
-            xmlwriter.WriteStartAttribute("ColorTextConsole", null); // Записывает атрибут с заданным именем
-            xmlwriter.WriteString(ColorTextConsole); // Записывает заданное текстовое содержимое атрибута
-            xmlwriter.WriteEndAttribute();
+                xmlwriter.WriteStartAttribute("ColorTextConsole", null); // Записывает атрибут с заданным именем
+                xmlwriter.WriteString(ColorTextConsole); // Записывает заданное текстовое содержимое атрибута
+                xmlwriter.WriteEndAttribute();
 
-            xmlwriter.WriteStartAttribute("size.H", null); // Записывает атрибут с заданным именем
-            xmlwriter.WriteString(size.H.ToString()); // Записывает заданное текстовое содержимое атрибута
-            xmlwriter.WriteEndAttribute();
+                xmlwriter.WriteStartAttribute("size.H", null); // Записывает атрибут с заданным именем
+                xmlwriter.WriteString(size.H.ToString()); // Записывает заданное текстовое содержимое атрибута
+                xmlwriter.WriteEndAttribute();
 
-            xmlwriter.WriteStartAttribute("size.W", null); // Записывает атрибут с заданным именем
-            xmlwriter.WriteString(size.W.ToString()); // Записывает заданное текстовое содержимое атрибута
-            xmlwriter.WriteEndAttribute();
+                xmlwriter.WriteStartAttribute("size.W", null); // Записывает атрибут с заданным именем
+                xmlwriter.WriteString(size.W.ToString()); // Записывает заданное текстовое содержимое атрибута
+                xmlwriter.WriteEndAttribute();
 
-            xmlwriter.WriteStartAttribute("StartTextConsole", null); // Записывает атрибут с заданным именем
-            xmlwriter.WriteString(StartTextConsole); // Записывает заданное текстовое содержимое атрибута
-            xmlwriter.WriteEndAttribute();
+                xmlwriter.WriteStartAttribute("StartTextConsole", null); // Записывает атрибут с заданным именем
+                xmlwriter.WriteString(StartTextConsole); // Записывает заданное текстовое содержимое атрибута
+                xmlwriter.WriteEndAttribute();
 
-            xmlwriter.WriteEndElement(); 
-            xmlwriter.WriteEndElement();
-            xmlwriter.Close();
-
+                xmlwriter.WriteEndElement();
+                xmlwriter.WriteEndElement();
+                xmlwriter.Close();
+            }
            
 
         }
@@ -72,10 +74,16 @@ namespace ConsoleSeting
             {
                 ColorFonConsole = (n.SelectSingleNode("@ColorFonConsole").Value);
                 Console.BackgroundColor = (ConsoleColor)Enum.Parse(typeof(ConsoleColor), ColorFonConsole);
+                Console.Clear();
                 ColorTextConsole = (n.SelectSingleNode("@ColorTextConsole").Value);
+                Console.ForegroundColor = (ConsoleColor)Enum.Parse(typeof(ConsoleColor), ColorTextConsole);
+
                 size.H = Int32.Parse(n.SelectSingleNode("@size.H").Value);
                 size.W = Int32.Parse(n.SelectSingleNode("@size.W").Value);
-                StartTextConsole = (n.SelectSingleNode("@StartTextConsole").Value);               
+                Console.SetWindowSize(size.W, size.H);
+
+                StartTextConsole = (n.SelectSingleNode("@StartTextConsole").Value);
+                Console.Title = StartTextConsole;
             }
         }
     }
