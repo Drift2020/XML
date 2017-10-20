2502e653e56874e048423abf90b2b5e09bb2db8c
Mamory <32417036+Drift2020@users.noreply.github.com>
2017-10-21 01:13:01 +0300

    FINIH

--- ConsoleSeting/Tehnic/Start.cs
+++ ConsoleSeting/Tehnic/Start.cs
@@ -3,14 +3,124 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using System.Xml;
 
 namespace Tehnic
 {
     class Start
     {
-        List<Storeg> list = new List<Storeg>();
+        static List<Storeg> list = new List<Storeg>();
+
+        static void Add()
+        {
+            NAME name;
+            Console.Write("Please, enter name of firm:");
+            name.name_of_firm= Console.ReadLine();
+
+            Console.Write("Please, enter model:");
+            name.model = Console.ReadLine();
+
+            Console.Write("Please, enter seria:");
+            name.seria = Console.ReadLine();
+
+            Console.Write("Please, enter number:");
+            name.number=Int32.Parse(Console.ReadLine());
+
+            MANUFACTURE manufacture;
+            Console.Write("Please, enter name of country:");
+            manufacture.name_of_country = Console.ReadLine();
+
+            Console.Write("Please, enter day:");
+            manufacture.date.day = Int32.Parse(Console.ReadLine());
+
+            Console.Write("Please, enter month:");
+            manufacture.date.month = Int32.Parse(Console.ReadLine());
+
+            Console.Write("Please, enter year:");
+            manufacture.date.year = Int32.Parse(Console.ReadLine());
+
+            CHARACTERISTICS characteristics;
+            Console.Write("Please, enter power:");
+            characteristics.power = Console.ReadLine();
+
+            Console.Write("Please, enter additional characteristics:");
+            characteristics.additional_characteristics = Console.ReadLine();
+
+            Console.Write("Please, enter main characteristics:");
+            characteristics.main_characteristics = Console.ReadLine();
+
+            PRICE price;
+            Console.Write("Please, enter price retail:");
+            price.price_retail = Int32.Parse(Console.ReadLine());
+
+            Console.Write("Please, enter price wholesale:");
+            price.price_wholesale = Int32.Parse(Console.ReadLine());
+
+            INFO_TO_CRED info_to_cred;
+            Console.Write("Please, enter count month:");
+            info_to_cred.count_month = Int32.Parse(Console.ReadLine());
+
+            Console.Write("Please, enter prise of month:");
+            info_to_cred.prise_of_month = Int32.Parse(Console.ReadLine());
+
+
+            Storeg temp = new Storeg(name, manufacture, characteristics, price, info_to_cred);
+            list.Add(temp);
+
+        }
+        static void PrintToXML()
+        {
+            // XmlTextWriter представляет средство записи, предоставляющее способ быстрого прямого создания XML-файлов 
+            XmlTextWriter xmlwriter = new XmlTextWriter("../../prise_liist.xml", Encoding.UTF8);
+            xmlwriter.WriteStartDocument();// Записывает объявление XML с номером версии 1.0
+            // Formatting определяет способ форматирования выходных данных
+            xmlwriter.Formatting = Formatting.Indented; //Форматирует отступы в дочерних элементах в соответствии с параметрами настройки IndentChar и Indentation
+
+
+            xmlwriter.IndentChar = '\t'; // Возвращает или задает знак для отступа
+            xmlwriter.Indentation = 1; // Возвращает или задает количество записываемых IndentChars для каждого уровня в иерархии
+
+            xmlwriter.WriteStartElement("technics"); // Записывает указанный открывающий тег
+          foreach(Storeg e in list)
+            {
+                e.PrintXML(xmlwriter);
+            }
+            
+
+
+            xmlwriter.WriteEndElement();
+            Console.WriteLine("Данные сохранены в XML-файл");
+            xmlwriter.Close();
+        }
+        static void PrintMenu()
+        {
+            Console.Write("1. Add\n2.Print to XML\nPlease, enter number:");
+        }
         static void Main(string[] args)
         {
+            try {
+                int number;
+                do {
+                    Console.Clear();
+                    PrintMenu();
+                    number =Int32.Parse(Console.ReadLine());
+                    switch(number)
+                    {
+                        case 1:
+                            Add();
+                            Console.Clear();
+                            break;
+                        case 2:
+                            PrintToXML();
+                            break;
+                    }
+                } while (true);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
         }
     }
 }
--- ConsoleSeting/Tehnic/Storeg.cs
+++ ConsoleSeting/Tehnic/Storeg.cs
@@ -3,50 +3,51 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
 
 namespace Tehnic
 {
     struct NAME
     {
-        string name_of_firm;
-        string model;
-        string seria;
-        int numbe;
+        public string name_of_firm;
+        public string model;
+        public string seria;
+        public int number;
     }
     struct DATE
     {
-        int day;
-        int month;
-        int year;
+        public int day;
+        public int month;
+        public int year;
     }
     struct MANUFACTURE
     {
-        string name_of_country;
-        DATE date;
+        public string name_of_country;
+        public  DATE date;
     }
     struct CHARACTERISTICS
     {
-        string power;
-        string additional_characteristics;
-        string main_characteristics;
+        public string power;
+        public string additional_characteristics;
+        public string main_characteristics;
     }
     struct PRICE
     {
-        int price_retail;
-        int price_wholesale;
+        public int price_retail;
+        public int price_wholesale;
     }
     struct INFO_TO_CRED
     {
-        string count_month;
-        string prise_of_month;
+        public int count_month;
+        public int prise_of_month;
     }
     class Storeg
     {
-        NAME name;
-        MANUFACTURE manufacture;
-        CHARACTERISTICS characteristics;
-        PRICE price;
-        INFO_TO_CRED info_to_cred;
+         NAME name;
+         MANUFACTURE manufacture;
+         CHARACTERISTICS characteristics;
+         PRICE price;
+         INFO_TO_CRED info_to_cred;
       public Storeg(NAME name, MANUFACTURE manufacture, CHARACTERISTICS characteristics, PRICE price, INFO_TO_CRED info_to_cred)
       {
             this.name = name;
@@ -56,9 +57,84 @@ namespace Tehnic
             this.info_to_cred = info_to_cred;
       }
       
-        public void PrintXML()
+        public void PrintXML(XmlTextWriter xmlwriter)
         {
+            xmlwriter.WriteStartElement("product");
 
+            xmlwriter.WriteStartAttribute("name_is_firm", null); // Записывает атрибут с заданным именем
+            xmlwriter.WriteString(name.name_of_firm); // Записывает заданное текстовое содержимое атрибута
+            xmlwriter.WriteEndAttribute(); // Закрывает атрибут
+
+            xmlwriter.WriteStartAttribute("model", null);
+            xmlwriter.WriteString(name.model);
+            xmlwriter.WriteEndAttribute();
+
+            xmlwriter.WriteStartElement("manufacture");
+           
+            xmlwriter.WriteStartAttribute("name_of_country", null);
+            xmlwriter.WriteString(manufacture.name_of_country);
+            xmlwriter.WriteEndAttribute();
+
+            xmlwriter.WriteStartElement("date");
+
+            xmlwriter.WriteStartAttribute("day", null);
+            xmlwriter.WriteString(manufacture.date.day.ToString());
+            xmlwriter.WriteEndAttribute();
+
+            xmlwriter.WriteStartAttribute("month", null);
+            xmlwriter.WriteString(manufacture.date.month.ToString());
+            xmlwriter.WriteEndAttribute();
+
+            xmlwriter.WriteStartAttribute("year", null);
+            xmlwriter.WriteString(manufacture.date.year.ToString());
+            xmlwriter.WriteEndAttribute();
+
+            xmlwriter.WriteEndElement();
+            xmlwriter.WriteEndElement();
+
+
+            xmlwriter.WriteStartElement("characteristics");
+
+            xmlwriter.WriteStartAttribute("power", null);
+            xmlwriter.WriteString(characteristics.power);
+            xmlwriter.WriteEndAttribute();
+
+            xmlwriter.WriteStartElement("additional_characteristics");
+            xmlwriter.WriteString(characteristics.additional_characteristics);
+            xmlwriter.WriteEndElement();
+
+            xmlwriter.WriteStartElement("main_characteristics");
+            xmlwriter.WriteString(characteristics.main_characteristics);
+            xmlwriter.WriteEndElement();
+
+            xmlwriter.WriteEndElement();
+
+            xmlwriter.WriteStartElement("price");
+
+            xmlwriter.WriteStartAttribute("price_retail", null);
+            xmlwriter.WriteString(price.price_retail.ToString());
+            xmlwriter.WriteEndAttribute();
+
+            xmlwriter.WriteStartAttribute("price_wholesale", null);
+            xmlwriter.WriteString(price.price_wholesale.ToString());
+            xmlwriter.WriteEndAttribute();
+
+            xmlwriter.WriteEndElement();
+
+            xmlwriter.WriteStartElement("info_to_cred");
+
+            xmlwriter.WriteStartAttribute("count_month", null);
+            xmlwriter.WriteString(info_to_cred.count_month.ToString());
+            xmlwriter.WriteEndAttribute();
+
+            xmlwriter.WriteStartAttribute("prise_of_month", null);
+            xmlwriter.WriteString(info_to_cred.prise_of_month.ToString());
+            xmlwriter.WriteEndAttribute();
+
+            xmlwriter.WriteEndElement();
+
+
+            xmlwriter.WriteEndElement(); // Закрывает один элемент
         }
 
     }
